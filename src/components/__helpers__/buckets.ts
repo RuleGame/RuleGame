2e668668dcf66ffbdd56450eccaa07fbfdf20fc2
Kevin Mui <kmui2@wisc.edu>
2019-07-15 22:56:29 -0500

    shape must be square for initial move

--- src/components/__helpers__/buckets.ts
+++ src/components/__helpers__/buckets.ts
@@ -11,16 +11,18 @@ export const setAllBucketsMapperCreator = (buckets: BucketPosition[]): BoardObje
 export const closestBucketsMapper: BoardObjectsMapper = (boardObject) => ({
   ...boardObject,
   buckets: new Set([
-    ...(boardObject.x <= cols / 2 && boardObject.y <= rows / 2 ? ['BL'] as const : []),
-    ...(boardObject.x >= cols / 2 && boardObject.y <= rows / 2 ? ['BR'] as const : []),
-    ...(boardObject.x >= cols / 2 && boardObject.y >= rows / 2 ? ['TR'] as const : []),
-    ...(boardObject.x <= cols / 2 && boardObject.y >= rows / 2 ? ['TL'] as const : []),
+    ...(boardObject.x <= cols / 2 && boardObject.y <= rows / 2 ? (['BL'] as const) : []),
+    ...(boardObject.x >= cols / 2 && boardObject.y <= rows / 2 ? (['BR'] as const) : []),
+    ...(boardObject.x >= cols / 2 && boardObject.y >= rows / 2 ? (['TR'] as const) : []),
+    ...(boardObject.x <= cols / 2 && boardObject.y >= rows / 2 ? (['TL'] as const) : []),
   ]),
 });
 
 export const initialBucketsMapper: BoardObjectsMapper = (boardObject) => ({
   ...boardObject,
   buckets: new Set(
-    boardObject.color === 'blue' ? (['TL', 'TR', 'BL', 'BR'] as const) : [],
+    boardObject.color === 'blue' && boardObject.shape === 'square'
+      ? (['TL', 'TR', 'BL', 'BR'] as const)
+      : [],
   ),
 });
