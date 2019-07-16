e2a1eee47c3d9b3cb08f744e89abe2f0cd00b52e
Kevin Mui <kmui2@wisc.edu>
2019-07-15 23:59:16 -0500

    switched from handleAction to switch cases

--- src/store/reducers/game.ts
+++ src/store/reducers/game.ts
@@ -1,12 +1,13 @@
-import { createReducer, ActionType } from 'typesafe-actions';
-import { initBoard, resetBoard, move, updateBoardObject } from '../actions/game';
+import { Reducer } from 'redux';
+import { ActionType, getType } from 'typesafe-actions';
+import { BoardObjectType, BucketPosition, Log, Rule } from '../../@types/index';
 import {
   blueSquareAnyBucket as blueSquareOnlyAnyBucket,
-  setAllBucketsTo,
   closestBucket,
+  setAllBucketsTo,
 } from '../../components/__helpers__/rule-set-mappers';
-import { initialBoardObjects, bucketOrder } from '../../constants/index';
-import { BucketPosition, BoardObjectType, Rule, Log } from '../../@types/index';
+import { bucketOrder, initialBoardObjects } from '../../constants/index';
+import { initBoard, move, resetBoard, updateBoardObject } from '../actions/game';
 
 export type Action = ActionType<typeof import('../actions/game')>;
 
@@ -18,7 +19,7 @@ type State = {
   logs: Log[];
 };
 
-export default createReducer<State, Action>({
+const initialState: State = {
   boardObjectsById: initialBoardObjects
     .map((mininmalBoardObjectType) => ({
       ...mininmalBoardObjectType,
@@ -37,94 +38,106 @@ export default createReducer<State, Action>({
   moveNum: 1,
   logs: [],
   rule: 'clockwise', // Note: Remember to update initial mapper based on initial rule!
-})
-  .handleAction(initBoard, (state, action) => ({
-    ...state,
-    boardObjectsById: Object.values(state.boardObjectsById)
-      .map(action.payload.firstMoveMapper)
-      .reduce(
-        (acc, curr) => ({
-          ...acc,
-          [curr.id]: curr,
-        }),
-        {},
-      ),
-    moveNum: 1,
-    boardId: state.boardId + 1,
-    rule: action.payload.rule,
-    logs: [],
-  }))
-  .handleAction(resetBoard, (state) => ({
-    ...state,
-    boardObjectsById: initialBoardObjects
-      .map((mininmalBoardObjectType) => ({
-        ...mininmalBoardObjectType,
-        buckets: new Set<BucketPosition>(),
-        draggable: true,
-      }))
-      .map(blueSquareOnlyAnyBucket)
-      .reduce(
-        (acc, curr) => ({
-          ...acc,
-          [curr.id]: curr,
-        }),
-        {},
-      ),
-    boardId: 0,
-    moveNum: 1,
-    logs: [],
-    rule: 'clockwise',
-  }))
-  .handleAction(move, (state, action) => {
-    let mapper;
-    switch (state.rule) {
-      case 'clockwise': {
-        const newIndex =
-          (bucketOrder.indexOf(action.payload.dropSuccess.dropped) + 1) % bucketOrder.length;
-        mapper = setAllBucketsTo([bucketOrder[newIndex]]);
-        break;
+};
+
+const reducer: Reducer<State, Action> = (state = initialState, action) => {
+  switch (action.type) {
+    case getType(initBoard):
+      return {
+        ...state,
+        boardObjectsById: Object.values(state.boardObjectsById)
+          .map(action.payload.firstMoveMapper)
+          .reduce(
+            (acc, curr) => ({
+              ...acc,
+              [curr.id]: curr,
+            }),
+            {},
+          ),
+        moveNum: 1,
+        boardId: state.boardId + 1,
+        rule: action.payload.rule,
+        logs: [],
+      };
+    case getType(resetBoard):
+      return {
+        ...state,
+        boardObjectsById: initialBoardObjects
+          .map((mininmalBoardObjectType) => ({
+            ...mininmalBoardObjectType,
+            buckets: new Set<BucketPosition>(),
+            draggable: true,
+          }))
+          .map(blueSquareOnlyAnyBucket)
+          .reduce(
+            (acc, curr) => ({
+              ...acc,
+              [curr.id]: curr,
+            }),
+            {},
+          ),
+        boardId: 0,
+        moveNum: 1,
+        logs: [],
+        rule: 'clockwise',
+      };
+    case getType(move): {
+      let mapper;
+      switch (state.rule) {
+        case 'clockwise': {
+          const newIndex =
+            (bucketOrder.indexOf(action.payload.dropSuccess.dropped) + 1) % bucketOrder.length;
+          mapper = setAllBucketsTo([bucketOrder[newIndex]]);
+          break;
+        }
+        case 'closest':
+          mapper = closestBucket;
+          break;
+        default:
+          mapper = (boardObject: BoardObjectType) => boardObject;
       }
-      case 'closest':
-        mapper = closestBucket;
-        break;
-      default:
-        mapper = (boardObject: BoardObjectType) => boardObject;
+      return {
+        ...state,
+        boardObjectsById: Object.values(state.boardObjectsById)
+          .map(mapper)
+          .reduce(
+            (acc, curr) => ({
+              ...acc,
+              [curr.id]: curr,
+            }),
+            {},
+          ),
+        moveNum: state.moveNum + 1,
+        logs: [
+          ...state.logs,
+          {
+            id: state.logs.length,
+            data: {
+              boardId: state.boardId,
+              moveNum: state.moveNum,
+              dropAttempts: action.payload.dropAttempts,
+              dropSuccess: action.payload.dropSuccess,
+              touchAttempts: action.payload.touchAttempts,
+            },
+          },
+        ],
+      };
     }
-    return {
-      ...state,
-      boardObjectsById: Object.values(state.boardObjectsById)
-        .map(mapper)
-        .reduce(
-          (acc, curr) => ({
-            ...acc,
-            [curr.id]: curr,
-          }),
-          {},
-        ),
-      moveNum: state.moveNum + 1,
-      logs: [
-        ...state.logs,
-        {
-          id: state.logs.length,
-          data: {
-            boardId: state.boardId,
-            moveNum: state.moveNum,
-            dropAttempts: action.payload.dropAttempts,
-            dropSuccess: action.payload.dropSuccess,
-            touchAttempts: action.payload.touchAttempts,
+    case getType(updateBoardObject):
+      return {
+        ...state,
+        boardObjectsById: {
+          ...state.boardObjectsById,
+          [action.payload.id]: {
+            ...state.boardObjectsById[action.payload.id],
+            ...action.payload.boardObject,
           },
         },
-      ],
-    };
-  })
-  .handleAction(updateBoardObject, (state, action) => ({
-    ...state,
-    boardObjectsById: {
-      ...state.boardObjectsById,
-      [action.payload.id]: {
-        ...state.boardObjectsById[action.payload.id],
-        ...action.payload.boardObject,
-      },
-    },
-    moveNum: state.moveNum + 1,
-  }));
+        moveNum: state.moveNum + 1,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
