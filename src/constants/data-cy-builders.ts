456b9380e1ca752a7dc10f6d903c825cbfd18ff0
Kevin Mui <kmui2@wisc.edu>
2019-11-04 00:45:49 -0600

    fix test

--- src/constants/data-cy-builders.ts
+++ src/constants/data-cy-builders.ts
@@ -1,4 +1,6 @@
-import { Shape } from '../@types';
+import { Game, Shape } from '../@types';
 
 export const shapeObjectCy = <T extends string = Shape>(id: number, shape: T) =>
   `shape-object-${id}-${shape}`;
+
+export const entranceButtonCy = (game: Game) => `game-${game}`;
