0be95671784e4f3804ea3e046c640c5155c78781
Kevin Mui <kmui2@wisc.edu>
2019-12-06 01:41:41 -0600

    :speech_balloon: Correct syntax text

--- src/utils/atom-parser.ts
+++ src/utils/atom-parser.ts
@@ -67,7 +67,7 @@ const parseAtomString = (atom: string): Atom => {
   const matches = regex.exec(atom);
   if (matches === null) {
     throw Error(
-      `Invalid atom syntax: ${atom}\n\nFYI:\nCorrect synatx: (counter,shape,color,[bucketFunction1,...])\nCorrect example: (10,*,green,10,[2,(pc+1)%4])\n\nRemember, spaces are only used for separating multiple atoms in the same row.\nExample:\n(10,square,*,*,[1,2]) (10,*,green,10,[2,3])`,
+      `Invalid atom syntax: ${atom}\n\nFYI:\nCorrect synatx: (counter,shape,color,position,[bucketFunction1,...])\nCorrect example: (10,*,green,10,[2,(pc+1)%4])\n\nRemember, spaces are only used for separating multiple atoms in the same row.\nExample:\n(10,square,*,*,[1,2]) (10,*,green,10,[2,3])`,
     );
   }
   const [, matchedCounter, matchedShape, matchedColor, matchedPosition, matchedFn] = matches;
