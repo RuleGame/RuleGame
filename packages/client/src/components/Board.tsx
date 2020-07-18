a738be72339197b20f505f8b2911fc6757b2ab01
Kevin Mui <kmui2@wisc.edu>
2020-07-18 14:37:57 -0500

    :lipstick: Add bucket drop list

--- packages/client/src/components/Board.tsx
+++ packages/client/src/components/Board.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useSelector } from 'react-redux';
+import { Box, Grid } from 'grommet';
 import { BoardObjectItem, BoardObjectType, BucketPosition, BucketType, Shape } from '../@types';
 import { buckets, cols, rows } from '../constants';
 import BoardObject from './BoardObject';
 import Bucket from './Bucket';
 import { droppedBucketSelector } from '../store/selectors';
+import BucketDropList from './BucketDropList';
 
 const StyledBoard = styled.div<{}>`
   display: grid;
@@ -45,6 +47,14 @@ type BoardProps = {
   boardObjectsToDebugInfo?: { [boardObjectId: string]: string };
 };
 
+enum GridAreaName {
+  TL = 'TL',
+  TR = 'TR',
+  BL = 'BL',
+  BR = 'BR',
+  BOARD = 'BOARD',
+}
+
 const Board = ({
   onDrop,
   boardObjects,
@@ -58,31 +68,80 @@ const Board = ({
   const droppedBucket = useSelector(droppedBucketSelector);
 
   return (
-    <StyledBoard className={className}>
-      {boardObjects.map((boardObject) => (
-        <StyledBoardObject
-          {...boardObject}
-          key={`${boardObject.x}-${boardObject.y}`}
-          item={{
-            ...boardObject,
-            type: 'object',
-            buckets: boardObjectsToBuckets[boardObject.id],
-            debugInfo: boardObjectsToDebugInfo && boardObjectsToDebugInfo[boardObject.id],
-          }}
-          onClick={() => !droppedBucket && onBoardObjectClick(boardObject)}
-        />
-      ))}
-      {buckets.map((bucket) => (
-        <StyledBucket
-          {...bucket}
-          key={`${bucket.x}-${bucket.y}`}
-          onDrop={onDrop(bucket)}
-          canDrop={() => !paused}
-          shape={droppedBucket === bucket.pos ? droppedBucketShape : Shape.BUCKET}
-          bucket={bucket}
-        />
-      ))}
-    </StyledBoard>
+    <Grid
+      className={className}
+      fill
+      rows={['10%', '40%', '40%', '10%']}
+      columns={['10%', '80%', '10%']}
+      areas={[
+        {
+          name: GridAreaName.TL,
+          start: [0, 0],
+          end: [0, 1],
+        },
+        {
+          name: GridAreaName.TR,
+          start: [2, 0],
+          end: [2, 1],
+        },
+        {
+          name: GridAreaName.BL,
+          start: [0, 2],
+          end: [0, 3],
+        },
+        {
+          name: GridAreaName.BR,
+          start: [2, 2],
+          end: [2, 3],
+        },
+        {
+          name: GridAreaName.BOARD,
+          start: [1, 1],
+          end: [1, 2],
+        },
+      ]}
+    >
+      <Box gridArea={GridAreaName.TL} fill justify="center" align="center">
+        <BucketDropList bucket={BucketPosition.TL} />
+      </Box>
+      <Box gridArea={GridAreaName.TR} fill justify="center" align="center">
+        <BucketDropList bucket={BucketPosition.TR} />
+      </Box>
+      <Box gridArea={GridAreaName.BL} fill justify="center" align="center">
+        <BucketDropList bucket={BucketPosition.BL} />
+      </Box>
+      <Box gridArea={GridAreaName.BR} fill justify="center" align="center">
+        <BucketDropList bucket={BucketPosition.BR} />
+      </Box>
+
+      <Box gridArea={GridAreaName.BOARD}>
+        <StyledBoard>
+          {boardObjects.map((boardObject) => (
+            <StyledBoardObject
+              {...boardObject}
+              key={`${boardObject.x}-${boardObject.y}`}
+              item={{
+                ...boardObject,
+                type: 'object',
+                buckets: boardObjectsToBuckets[boardObject.id],
+                debugInfo: boardObjectsToDebugInfo && boardObjectsToDebugInfo[boardObject.id],
+              }}
+              onClick={() => !droppedBucket && onBoardObjectClick(boardObject)}
+            />
+          ))}
+          {buckets.map((bucket) => (
+            <StyledBucket
+              {...bucket}
+              key={`${bucket.x}-${bucket.y}`}
+              onDrop={onDrop(bucket)}
+              canDrop={() => !paused}
+              shape={droppedBucket === bucket.pos ? droppedBucketShape : Shape.BUCKET}
+              bucket={bucket}
+            />
+          ))}
+        </StyledBoard>
+      </Box>
+    </Grid>
   );
 };
 
---
+++ packages/client/src/components/BucketDropList.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Box } from 'grommet';
+import { BucketPosition } from '../@types';
+import { bucketDropListSelector } from '../store/selectors/rule-row';
+import ShapeObject from './ShapeObject';
+
+const BucketDropList: React.FunctionComponent<{
+  bucket: BucketPosition;
+}> = ({ bucket }) => {
+  const bucketDropList = useSelector(bucketDropListSelector(bucket));
+  return (
+    <Box
+      direction="column-reverse"
+      justify="start"
+      width="50%"
+      height="90%"
+      border={[{ side: 'bottom' }, { side: 'left' }, { side: 'right' }]}
+    >
+      {bucketDropList.map((boardObject) => (
+        <Box fill="horizontal" height="min-content">
+          <ShapeObject shape={boardObject.shape} color={boardObject.color} />
+        </Box>
+      ))}
+    </Box>
+  );
+};
+
+export default BucketDropList;
