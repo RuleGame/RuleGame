db39211927350540655417d685fe9081bcf0f3e0
Kevin Mui <kmui2@wisc.edu>
2020-08-03 23:50:31 -0500

    :rocket: Deploy on Sapir

--- packages/client/src/components/GuessRuleForm.tsx
+++ packages/client/src/components/GuessRuleForm.tsx
@@ -1,19 +1,30 @@
 import React, { useState } from 'react';
 import { Button, Form, FormField, Heading, TextInput } from 'grommet';
-import { RULE_EMAIL_ADDRESS } from '../constants';
+import { useSelector } from 'react-redux';
+import { playerNameSelector } from '../store/selectors/history';
 
 const GuessRuleForm: React.FunctionComponent<{ gameId: string; onSubmit?: () => void }> = ({
   gameId,
   onSubmit = () => {},
 }) => {
   const [ruleGuess, setRuleGuess] = useState('');
+  const playerName = useSelector(playerNameSelector);
 
   return (
     <Form
       onSubmit={() => {
-        const a = document.createElement('a');
-        a.href = `mailto:${RULE_EMAIL_ADDRESS}?subject=Rule Array Guess (GameID: ${gameId})&body=${ruleGuess}`;
-        a.click();
+        const formData = new FormData();
+        formData.append('dir', 'guesses');
+        formData.append('file', `${playerName}_${gameId}_${Date.now()}.txt`);
+        formData.append('data', ruleGuess);
+
+        fetch('http://sapir.psych.wisc.edu:7150/w2020/game-data/GameService/writeFile', {
+          method: 'POST',
+          body: formData,
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        });
         onSubmit();
       }}
     >
