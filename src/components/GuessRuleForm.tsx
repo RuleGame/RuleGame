5495d5f5f426276b59203188f2ae638cd119c20e
Kevin Mui <kmui2@wisc.edu>
2020-09-22 19:43:42 -0500

    :lipstick: Fix board dimensions

--- src/components/GuessRuleForm.tsx
+++ src/components/GuessRuleForm.tsx
@@ -37,11 +37,10 @@ const GuessRuleForm: React.FunctionComponent = () => {
   const [autofillButtonOver, setAutofillButtonOver] = useState(false);
 
   const isPrevSeriesRuleGuessSaved = seriesNo === savedSeriesNo && savedRuleGuess !== undefined;
+  const isRuleGuessEmpty = ruleGuess.trim().length === 0;
 
-  // Form type should accept generics for submit element and onChange parameter
-  // FireFox needs height={{ min: 'unset' }} inside a grid
   return guessOpened ? (
-    <Box height={{ min: 'unset' }}>
+    <>
       <Form onSubmit={() => setShowScale(true)}>
         <Box fill>
           <Box align="start" direction="row" justify="center" height={{ min: 'unset' }}>
@@ -135,7 +134,7 @@ const GuessRuleForm: React.FunctionComponent = () => {
       {showScale && (
         <Grid
           align="center"
-          columns={['auto', 'auto', 'auto']}
+          columns={['auto', 'min-content', 'auto']}
           rows={['auto', 'auto']}
           areas={[
             { name: GridArea.PROMPT, start: [1, 0], end: [1, 0] },
@@ -155,13 +154,16 @@ const GuessRuleForm: React.FunctionComponent = () => {
               end: [2, 1],
             },
           ]}
+          fill
         >
           <Box align="center" gridArea={GridArea.PROMPT}>
-            <Heading level="2">How sure are you?</Heading>
+            <Text color={isRuleGuessEmpty ? 'gray' : 'black'}>
+              <Heading level="2">How sure are you?</Heading>
+            </Text>
           </Box>
           <Box fill="vertical" gridArea={GridArea.LEAST} align="end">
             <Box width="min-content">
-              <Text textAlign="end">
+              <Text textAlign="end" color={isRuleGuessEmpty ? 'gray' : 'black'}>
                 <Heading level="3" margin="none">
                   Just guessing
                 </Heading>
@@ -178,7 +180,7 @@ const GuessRuleForm: React.FunctionComponent = () => {
             {range(1, scaleSize + 1).map((ratingNum) => (
               <Box justify="center" align="center" key={ratingNum}>
                 <Button
-                  disabled={ruleGuess.trim().length === 0}
+                  disabled={isRuleGuessEmpty}
                   rating-num={RATING_NUM_ATTRIBUTE_VALUE}
                   label={
                     <Box align="center" justify="center">
@@ -206,7 +208,7 @@ const GuessRuleForm: React.FunctionComponent = () => {
           </Box>
           <Box fill="vertical" width="min-content" gridArea={GridArea.GREATEST}>
             <Box width="min-content">
-              <Text textAlign="start">
+              <Text textAlign="start" color={isRuleGuessEmpty ? 'gray' : 'black'}>
                 <Heading level="3" margin="none">
                   Completely sure
                 </Heading>
@@ -228,7 +230,7 @@ const GuessRuleForm: React.FunctionComponent = () => {
           </Box>
         </Drop>
       )}
-    </Box>
+    </>
   ) : (
     // FireFox needs height={{ min: 'unset' }} inside a grid
     <Box width="20em" fill height={{ min: 'unset' }} align="center" margin="small">
