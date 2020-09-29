9245ca986ec7604f47953c53d5046a4e6db38252
Kevin Mui <kmui2@wisc.edu>
2020-09-28 20:55:57 -0500

    :sparkles: Add fullscreen requirement page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import { Grommet, Paragraph } from 'grommet';
+import React, { useRef, useState } from 'react';
+import { Box, Button, Grommet, Heading, Paragraph } from 'grommet';
 import { hot } from 'react-hot-loader/root';
 import { useSelector } from 'react-redux';
+import { useEvent, useSearchParam } from 'react-use';
+import { MdFullscreen } from 'react-icons/all';
+import { Expand } from 'grommet-icons';
 import Layers from './components/Layers';
 import Notifications from './components/Notifications';
 import Introduction from './pages/Introduction';
@@ -13,31 +16,58 @@ import LoadingTrials from './pages/LoadingTrials';
 import { pageSelector } from './store/selectors/page';
 import Demographics from './pages/Demographics';
 import Debriefing from './pages/Debriefing';
+import { SEARCH_QUERY_KEYS } from './constants';
 
 const App = () => {
   const page = useSelector(pageSelector);
+  const ref = useRef<HTMLDivElement>(null);
+  const requireFullscreen = useSearchParam(SEARCH_QUERY_KEYS.FULLSCREEN)?.toLowerCase() === 'true';
+  const [fullscreen, setFullscreen] = useState(false);
+  useEvent('fullscreenchange', () => {
+    if (document.fullscreenElement !== null) {
+      setFullscreen(true);
+    } else {
+      setFullscreen(false);
+    }
+  });
 
   return (
     <Grommet full plain>
-      {page === Page.INTRODUCTION ? (
-        <Introduction />
-      ) : page === Page.CONSENT ? (
-        <Consent />
-      ) : page === Page.LOADING_TRIALS ? (
-        <LoadingTrials />
-      ) : page === Page.TRIALS ? (
-        <Trials />
-      ) : page === Page.DEMOGRAPHICS_INSTRUCTIONS ? (
-        <DemographicsInstructions />
-      ) : page === Page.DEMOGRAPHICS ? (
-        <Demographics />
-      ) : page === Page.DEBRIEFING ? (
-        <Debriefing />
-      ) : (
-        <Paragraph>Unknown page. Try reloading</Paragraph>
-      )}
-      <Layers />
-      <Notifications />
+      <Box ref={ref} fill>
+        {!requireFullscreen || (requireFullscreen && fullscreen) ? (
+          page === Page.INTRODUCTION ? (
+            <Introduction />
+          ) : page === Page.CONSENT ? (
+            <Consent />
+          ) : page === Page.LOADING_TRIALS ? (
+            <LoadingTrials />
+          ) : page === Page.TRIALS ? (
+            <Trials />
+          ) : page === Page.DEMOGRAPHICS_INSTRUCTIONS ? (
+            <DemographicsInstructions />
+          ) : page === Page.DEMOGRAPHICS ? (
+            <Demographics />
+          ) : page === Page.DEBRIEFING ? (
+            <Debriefing />
+          ) : (
+            <Paragraph>Unknown page. Try reloading</Paragraph>
+          )
+        ) : (
+          <Box margin="xlarge" align="center">
+            <Expand />
+            <Heading>Fullscreen is required</Heading>
+            <Button
+              onClick={() => document.documentElement.requestFullscreen()}
+              primary
+              label="Enter fullscreen"
+              icon={<MdFullscreen />}
+            />
+          </Box>
+        )}
+
+        <Layers />
+        <Notifications />
+      </Box>
     </Grommet>
   );
 };
