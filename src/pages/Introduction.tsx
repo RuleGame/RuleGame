72bcad771af814ad5537f82115eb575bb93cbfe5
Kevin Mui <kmui2@wisc.edu>
2020-10-01 21:41:20 -0500

    :sparkles: Allow multi-page instructions

--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -1,34 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Button } from 'grommet';
 import { useDispatch } from 'react-redux';
 import { Dispatch } from 'redux';
 import { useQuery } from 'react-query';
 import { RootAction } from '../store/actions';
-import { nextPage } from '../store/actions/page';
 import { api, METHOD } from '../utils/api';
 import { useExperimentPlan, useWorkerId } from '../utils/hooks';
 import texts from '../constants/texts';
 import { Page } from '../constants/Page';
+import { nextPage } from '../store/actions/page';
 
 export default () => {
   const dispatch: Dispatch<RootAction> = useDispatch();
   const workerId = useWorkerId();
   const exp = useExperimentPlan();
+  const [step, setStep] = useState(0);
   const { data, isLoading } = useQuery(`${workerId}-INTRODUCTION`, () =>
     api('/w2020/game-data/GameService2/player', METHOD.POST, { playerId: workerId, exp }, {}),
   );
   const numRules = isLoading ? '...' : data?.data.trialList.length ?? '?';
+  const instructions = texts[Page.INTRODUCTION].text(numRules);
 
   return (
     <Box direction="column" align="center" gap="medium" pad="medium">
       <Box align="center" elevation="large" fill>
         <Box background="brand" fill align="center" pad="medium" justify="center">
-          {texts[Page.INTRODUCTION].text(numRules)}
-          <Button
-            label={texts[Page.INTRODUCTION].buttonLabel}
-            primary
-            onClick={() => dispatch(nextPage())}
-          />
+          {instructions[step]}
+          <Box direction="row" gap="small" justify="center">
+            <Button
+              label={texts[Page.INTRODUCTION].backButtonLabel}
+              disabled={step === 0}
+              onClick={() => setStep(step - 1)}
+            />
+            <Button
+              label={
+                step === instructions.length - 1
+                  ? texts[Page.INTRODUCTION].startExperimentButtonLabel
+                  : texts[Page.INTRODUCTION].nextButtonLabel
+              }
+              primary
+              onClick={() => {
+                if (step === instructions.length - 1) {
+                  dispatch(nextPage());
+                } else {
+                  setStep((step) => step + 1);
+                }
+              }}
+            />
+          </Box>
         </Box>
       </Box>
     </Box>
