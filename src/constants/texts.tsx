cf5f4c94d037b34c1747a9e895791f45485662c0
Gary Lupyan <glupyan@gmail.com>
2020-10-22 22:32:35 -0500

    Removed first instruction screen - redundant

--- src/constants/texts.tsx
+++ src/constants/texts.tsx
@@ -70,22 +70,6 @@ export default {
       // A list of JSX (HTML). One per instruction page.
       // Players can navigate through them via back and next buttons.
       [
-        // Page 1
-        <>
-          <Heading>RuleGame Challenge</Heading>
-          <Paragraph fill>
-            Welcome to the RuleGame challenge. There are {numRules} different rules. Each rule
-            describes an allowed way of clearing some <b>colored objects</b> from a game board. To
-            clear an object, you must grab it with the mouse, and drag it to the correct bucket.
-            When you release it at the correct bucket, the bucket smiles, and the object’s place
-            turns into a check mark. If you release it at the wrong bucket, the bucket makes an ugly
-            face. After you have cleared an entire display, you can give up, or ask for a new
-            display. After you have cleared a few displays without too many errors, you can take a
-            chance to guess at the rule. Please be patient after you guess because we require human
-            judges to review your guesses.
-          </Paragraph>
-        </>,
-
         // Page 2
         <>
           <Heading>RuleGame Challenge</Heading>
