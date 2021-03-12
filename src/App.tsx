ee6a596910c9898118ec25242be33d9318d759b0
Kevin Mui <kmui2@wisc.edu>
2021-03-11 22:25:46 -0600

    :sparkles: Add version page and refactor REACT_APP_APP_API_HOST_ORIGIN to include /w2020

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useRef, useState } from 'react';
-import { Box, Button, Grommet, Heading, Paragraph } from 'grommet';
+import { Box, Button, Grommet, Heading, Paragraph, Text } from 'grommet';
 import { hot } from 'react-hot-loader/root';
 import { useSelector } from 'react-redux';
 import { useEvent, useSearchParam } from 'react-use';
 import { MdFullscreen } from 'react-icons/all';
 import { Expand } from 'grommet-icons';
+import { useQuery } from 'react-query';
 import Layers from './components/Layers';
 import Notifications from './components/Notifications';
 import Introduction from './pages/Introduction';
@@ -16,13 +17,16 @@ import LoadingTrials from './pages/LoadingTrials';
 import { pageSelector } from './store/selectors/page';
 import Demographics from './pages/Demographics';
 import Debriefing from './pages/Debriefing';
-import { SEARCH_QUERY_KEYS } from './constants';
+import { API_HOST_ORIGIN, SEARCH_QUERY_KEYS, VERSION } from './constants';
 import texts from './constants/texts';
+import { api, METHOD } from './utils/api';
+import Spinner from './components/Spinner';
 
 const App = () => {
   const page = useSelector(pageSelector);
   const ref = useRef<HTMLDivElement>(null);
   const requireFullscreen = useSearchParam(SEARCH_QUERY_KEYS.FULLSCREEN)?.toLowerCase() === 'true';
+  const versionPage = useSearchParam(SEARCH_QUERY_KEYS.VERSION)?.toLowerCase() === 'true';
   const [fullscreen, setFullscreen] = useState(false);
   useEvent('fullscreenchange', () => {
     if (document.fullscreenElement !== null) {
@@ -32,10 +36,36 @@ const App = () => {
     }
   });
 
+  const { data: versionData, isLoading: isVersionLoading } = useQuery(
+    'VERSION',
+    () => api('/game-data/GameService2/getVersion', METHOD.GET, undefined, {}),
+    { enabled: versionPage, retry: false },
+  );
+
   return (
     <Grommet full plain>
       <Box ref={ref} fill>
-        {!requireFullscreen || (requireFullscreen && fullscreen) ? (
+        {versionPage ? (
+          <Box>
+            <Text>
+              Client Version [environment-commitHash]:{' '}
+              <Text weight="bold">{VERSION ?? 'Missing Client Version'}</Text>
+            </Text>
+            <Text>
+              Server URL: <Text weight="bold">{API_HOST_ORIGIN ?? 'Missing Server URL'}</Text>
+            </Text>
+            <Text>
+              Server Version:{' '}
+              <Text weight="bold">
+                {isVersionLoading ? (
+                  <Spinner />
+                ) : (
+                  versionData?.data ?? 'Could Not Fetch Server Version'
+                )}
+              </Text>
+            </Text>
+          </Box>
+        ) : !requireFullscreen || (requireFullscreen && fullscreen) ? (
           page === Page.INTRODUCTION ? (
             <Introduction />
           ) : page === Page.CONSENT ? (
