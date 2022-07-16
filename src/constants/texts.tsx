727958f24b8430d0a5c09fe422b3fe0e854a8c66
Kevin Mui <kmui2@wisc.edu>
2022-07-16 03:32:14 -0500

    Use objectFit: 'contain' to prevent instructions image from overflowing

--- src/constants/texts.tsx
+++ src/constants/texts.tsx
@@ -274,7 +274,7 @@ const INSTRUCTIONS_11 = [
   <>
     <Heading>RuleGame Challenge</Heading>
     <Box width="xlarge" align="center">
-      <Image src={src} width="100%" />
+      <Image src={src} width="100%" height="100%" style={{ objectFit: 'contain' }} />
     </Box>
   </>
 ));
