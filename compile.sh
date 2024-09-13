e6538fe34fef4adcc1f3bdaddcd5fbfa76046615
Vladimir Menkov <vmenkov@gmail.com>
2024-09-12 23:20:13 -0400

    compile script

---
+++ compile.sh
@@ -0,0 +1,25 @@
+#!/bin/csh
+
+# npm install
+
+setenv REACT_APP_APP_API_HOST_ORIGIN /w2020
+
+rm -rf rule-game 
+mkdir rule-game
+
+setenv REACT_APP_DEBUG_MODE_ENABLED true
+echo "Compiling with REACT_APP_DEBUG_MODE_ENABLED=$REACT_APP_DEBUG_MODE_ENABLED"
+# setenv REACT_APP_APP_API_HOST_ORIGIN http://localhost:8080/w2020
+rm -rf build 
+npm run build
+mv build rule-game/dev
+
+echo "Compiling with REACT_APP_DEBUG_MODE_ENABLED=$REACT_APP_DEBUG_MODE_ENABLED"
+setenv REACT_APP_DEBUG_MODE_ENABLED false
+# setenv REACT_APP_APP_API_HOST_ORIGIN http://localhost:8080/w2020
+rm -rf build
+npm run build
+mv build rule-game/prod
+
+rm -f rule-game.war
+(cd rule-game; jar -cvf ../rule-game.war .)
